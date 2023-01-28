b3d66362761ece4d92f714d369fcf678c6569da3
Got <dulyapat.ryk@gmail.com>
2023-01-28 19:15:57 +0700

    feat: add slick

--- script/components/slick.js
+++ script/components/slick.js
@@ -1,11 +1,27 @@
-$('.slick--image').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    infinite:true,
-    loop:true,
-    dots: false,
-    autoplay:true,
-    draggable:true,
-});  
+if(document.querySelector('.slick--image')) {
+    $('.slick--image').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        infinite:true,
+        loop:true,
+        dots: false,
+        autoplay:true,
+        draggable:true,
+    }); 
+}
+
+if(document.querySelector('.slick--portfolio')) { 
+    $('.slick--portfolio').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        infinite:true,
+        loop:true,
+        dots: false,
+        autoplay:true,
+        draggable:true,
+    });  
+}
